8e4da49013d756808ceb22b965a427c83e1c33db
ElektrazCode <ElektrazCode@gmail.com>
2022-09-21 18:03:53 -0400

    Added MongoDB Connection

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");                     // imports the express package, which facilitates server creation and handling
 const app = express();                                  // makes an instance of express and storing it in a constant app
 const logger = require("morgan");                       // imports the morgan package, which allows http request logging
+const mongoose = require("mongoose");                   // imports the mongoose package, which helps us create a schema for our data and communicating with MongoDB
 const mainRoutes = require("./routes/main");            // imports the main routes from the routes/main file 
+const connectDB = require("./config/database");
 
-require("dotenv").config({ path: "./config/.env" });    //Use .env file in config folder to keep all sensitive information secure 
+connectDB();                                            //Connect To Database
 
 app.set("view engine", "ejs");                          //Using EJS as a templating language for rendering views 
 
